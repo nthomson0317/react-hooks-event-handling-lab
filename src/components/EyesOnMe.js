b89f6a5657979804c85fa016e4827395232a5e01
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-05-18 17:56:55 -0400

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+let eyesOnMe = () => {
+    let focusClick = (event) => {
+        console.log('Good!')
+    }
+
+    let blurClick = (event) => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    return(
+        <button onFocus={focusClick} onBlur={blurClick}>Eyes on me</button>
+    )
+}
+
+export default eyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React from 'react'
+
+let keyPad = () => {
+    let handleChange = (event) => {
+        event.preventDefault()
+        console.log('Entering password...')
+    }
+
+    return (
+        <input onChange={handleChange} type="password"></input>
+    )
+}
+
+export default keyPad
